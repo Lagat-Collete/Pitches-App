15b922bd78e173b8294cda7374d1b394d05ec7ad
colletemine <jepngetichcolleta@gmail.com>
2022-05-09 07:54:05 +0300

    ft:Initializing SQLAlchemy

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#extension
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+  # Initializing flask extensions
+    db.init_app(app)
---
+++ manage.py
@@ -0,0 +1,9 @@
+
+from main.models import User
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+if __name__ == '__main__':
+    manager.run()
